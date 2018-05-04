dff99bb415bfabdb7adb3924097216691e6dbc8a
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-04 16:05:57 -0700

    Adding Primitives and Objects example

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 //FUNCTION CONSTRUCTOR
 
-//OBJECT.CREATE
-var personProto = {
-    calculateAge: function(){
-        console.log(2018 - this.yearOfBirth);
-    }
+//PRIMITIVES VS OBJECTS\
+
+//Primitives
+var a = 69;
+var b = a;
+a = 33;
+
+console.log(b); //69
+console.log(a); //33
+
+//Objects
+var obj1 = {
+    name: "jack",
+    age: 91
 };
 
-var john = Object.create(personProto);
-john.name = "John";
-john.yearOfBirth = 1995;
-john.job = "Plumber";
+var obj2 = obj1;
+obj1.age = 32;
 
-var emily = Object.create(personProto, {
-    name: { value: "Emily" },
-    yearOfBirth: { value: 69 },
-    job: { value: "Artist" }
-});
+console.log(obj1.age);  //30
+console.log(obj2.age);  //30
+
+//Functions
+var age = 22;
+var obj3 = {
+    name: "Miguel",
+    city: "Sacramento"
+};
+
+function change(a, b){
+    a = 23;
+    b.city = "Nigeria";
+}
+
+change(age, obj3);
+
+console.log(obj3);
+console.log(age);
